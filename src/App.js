d7235c68396a37bdc93cc9020e391982dae96afb
eskyi <thomson.ngo@icloud.com>
2023-12-06 15:21:18 +1100

    Cloudflare Turnstile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import axios from "axios";
-import { Turnstile } from "@marsidev/react-turnstile";
+import { Turnstile } from "react-turnstile";
 
 const App = () => {
   const [link, setLink] = useState("");
